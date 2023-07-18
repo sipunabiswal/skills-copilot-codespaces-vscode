476fd3a49edd2b25aaef9137c394efeea9a5b782
TheDevil <sipuna.biswal@gmail.com>
2023-07-18 16:45:08 +0000

    second commit

--- member.js
+++ member.js
@@ -6,6 +6,7 @@ function skillmember(){
         controllerAs: 'skillmember',
         bindToController: true,
         scope: {
-            member: '='}
+            member: '='
+        }
     };
 }
