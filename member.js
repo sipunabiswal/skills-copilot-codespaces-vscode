6ffae2ef32572d89c4f0b7166cad73a6349dde6b
TheDevil <sipuna.biswal@gmail.com>
2023-07-18 17:18:07 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,8 @@
-function skillmember(){
-    return{
-        restrict: 'E',
-        templateUrl: 'app/views/skillmember.html',
-        controller: 'skillmemberCtrl',
-        controllerAs: 'skillmember',
-        bindToController: true,
-        scope: {
-            member: '='
-        }
-    };
-};
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+}
